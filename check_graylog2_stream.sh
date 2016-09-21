3e429e69e687590438e1becc509702aa5fbfbd6a
Valentin Baraise <valentin.baraise@nameshield.net>
2016-09-21 09:17:01 +0200

    Working with jq and graylog 2.1.x

--- check_graylog2_stream.sh
+++ check_graylog2_stream.sh
@@ -8,16 +8,15 @@
 # You should have received a copy of the GNU General Public License along with Emind Systems DevOps Toolset. If not, see http://www.gnu.org/licenses.
 
 SERVER_URL=""
-GRAYLOG_USER=""
-GRAYLOG_PWD=""
+API_KEY=""
 TIME_DIFF=""
-STREAM_ID=""
+STREAM_NAME=""
 INVERT="OFF"
 ALARM_STATE="UNKNOWN"
-SSL="OFF"
+SSL="ON"
 
 function check_jq {
-    jshon_cmd=`which jq`
+    jq_cmd=`which jq`
     if [ ! -e ${jq_cmd} ]; then
         echo "jq not installed"
         exit 95
@@ -31,9 +30,9 @@ function write_log (){
 function get_streams_json()
 {
     if [ ${SSL} = "OFF" ]; then
-		curl -s -o ${2} ${1}
+		curl -s -o ${2} ${3} ${1}
     elif [ ${SSL} = "ON" ]; then
-		curl -k -s -o ${2} ${1}
+		curl -k -s -o ${2} ${3} ${1}
     fi
 
 	OUT=$?
@@ -50,13 +49,14 @@ function get_stream_index
     STREAM_INDEX=0
     CURRENT_STREAM_ID=""
     SEARCH_STATUS=false
-	FIRST_STREAM_ID=$(echo ${GRAYLOG2_STREAMS_FILE} | jq ".streams[${TARGET_STREAM_ID}].id")
+	  FIRST_STREAM_ID=$(cat ${GRAYLOG2_STREAMS_FILE} | jq -r ".streams[${STREAM_INDEX}].id")
     write_log "FIRST_STREAM_ID=${FIRST_STREAM_ID}"
     while true; do
-		CURRENT_STREAM_ID=$(jshon -e ${STREAM_INDEX} -e "title" -u < ${GRAYLOG2_STREAMS_FILE})
-    CURRENT_STREAM_ID=$(echo ${GRAYLOG2_STREAMS_FILE} | jq ".streams[${TARGET_STREAM_ID}].id")
-        if [ "${TARGET_STREAM_ID}" == "${CURRENT_STREAM_ID}" ]; then
+		#CURRENT_STREAM_ID=$(jq -e ${STREAM_INDEX} -e "title" -u < ${GRAYLOG2_STREAMS_FILE})
+    CURRENT_STREAM_ID=$(cat ${GRAYLOG2_STREAMS_FILE} | jq -r ".streams[${STREAM_INDEX}].id")
+    if [ "${TARGET_STREAM_ID}" == "${CURRENT_STREAM_ID}" ]; then
 			SEARCH_STATUS=true
+
 			break
 		elif [ ${STREAM_INDEX} -gt 0 ] && [ "${CURRENT_STREAM_ID}" == "${FIRST_STREAM_ID}" ]; then
 			break
@@ -70,13 +70,15 @@ function get_perf_count()
 {
 	VALUE=0
 
-	OBJECT_COUNT=$(jshon -l < ${1})
+	OBJECT_COUNT=$(jq -r '.alerts | length' < ${1})
 	if [ ${OBJECT_COUNT} -gt 0 ]; then
 	    LAST_OBJECT_IDX=$(expr ${OBJECT_COUNT} - 1)
-	    YOUNGEST_OBJECT_TIMESTAMP=$(jshon -e 0 -e "created_at" -u < ${1})
-	    OLDEST_OBJECT_TIMESTAMP=$(jshon -e ${LAST_OBJECT_IDX} -e "created_at" -u < ${1})
+	    YOUNGEST_OBJECT_TIMESTAMP=$(jq -r '.alerts[0].triggered_at' < ${1})
+	    OLDEST_OBJECT_TIMESTAMP=$(jq -r ".alerts[${LAST_OBJECT_IDX}].triggered_at" < ${1})
 
 	    #Time difference in seconds between first and last object
+      YOUNGEST_OBJECT_TIMESTAMP=$(date -d"${YOUNGEST_OBJECT_TIMESTAMP}" +%s)
+      OLDEST_OBJECT_TIMESTAMP=$(date -d"${OLDEST_OBJECT_TIMESTAMP}" +%s)
 	    TIME_DIFF=$(echo "scale=0; ${YOUNGEST_OBJECT_TIMESTAMP} - ${OLDEST_OBJECT_TIMESTAMP}" | bc -l)
 
 	    #Make sure not to divide by 0
@@ -90,22 +92,19 @@ function get_perf_count()
 	echo $VALUE
 }
 
-while getopts ig:u:p:t:s:c flag; do
+while getopts i:g:k:t:s:c flag; do
 case $flag in
     g)
         SERVER_URL=$OPTARG
         ;;
-    u)
-        GRAYLOG_USER=$OPTARG
-        ;;
-    p)
-        GRAYLOG_PWD=$OPTARG
+    k)
+        API_KEY=$OPTARG
         ;;
     t)
         TIME_DIFF=$OPTARG
         ;;
     s)
-        STREAM_ID=$OPTARG
+        STREAM_NAME=$OPTARG
         ;;
     i)
         INVERT="ON"
@@ -116,35 +115,37 @@ case $flag in
   esac
 done
 
-if [ "x${SERVER_URL}" == "x" ] || [ "x${GRAYLOG_USER}" == "x" ] || [ "x${GRAYLOG_PWD}" == "x" ] || [ "x${TIME_DIFF}" == "x" ] || [ "x${STREAM_ID}" == "x" ]; then
+if [ "x${SERVER_URL}" == "x" ] || [ "x${API_KEY}" == "x" ] || [ "x${TIME_DIFF}" == "x" ] || [ "x${STREAM_NAME}" == "x" ]; then
 echo "Missing input parameter"
-        echo "Usage: $0 -g <graylog server url> -u <graylog username> -p <graylog password> -t <alarm age> -s <stream id>"
+        echo "Usage: $0 -g <graylog server url> -k <graylog api_key> -t <alarm age> -s <stream name>"
         exit 96
 fi
 
 check_jq
 
-write_log "CMD Params: -g ${SERVER_URL} -t ${TIME_DIFF} -s ${STREAM_ID}"
+write_log "CMD Params: -g ${SERVER_URL} -k ${API_KEY} -t ${TIME_DIFF} -s ${STREAM_NAME}"
 
-STREAM_URL="http://${GRAYLOG_USER}:${GRAYLOG_PWD}@${SERVER_URL}/streams}"
+STREAM_URL="${SERVER_URL}/streams"
 GRAYLOG2_STREAMS_FILE="/tmp/$$.json"
 
-get_streams_json "${STREAM_URL}" "${GRAYLOG2_STREAMS_FILE}"
-get_stream_index "${STREAM_ID}"
+get_streams_json "${STREAM_URL}" "${GRAYLOG2_STREAMS_FILE}" "-u ${API_KEY}:token"
+get_stream_index ${STREAM_NAME}
+STREAM_ID=$(jq -r ".streams[${STREAM_INDEX}].id" < ${GRAYLOG2_STREAMS_FILE} 2> /dev/null)
 
-LAST_ALARM=$(jshon -e ${STREAM_INDEX} -e "last_alarm" -u < ${GRAYLOG2_STREAMS_FILE} 2> /dev/null)
-STREAM_ID=$(jshon -e ${STREAM_INDEX} -e "_id" -u < ${GRAYLOG2_STREAMS_FILE} 2> /dev/null)
+LAST_ALARM=$(curl -s -k -u 11b9i6j0lteqpv30gr7l81pp51lt7d6l3ehd3jm81sahfi4be4d6:token https://an-log-v1-nd.angers.perf1.net:443/api/streams/${STREAM_ID}/alerts?since=600 | jq -r ".alerts[0].triggered_at")
+
 rm -rf ${GRAYLOG2_STREAMS_FILE}
 
 if [ ${SEARCH_STATUS} == false ]; then
-echo "Unknown Stream:${STREAM_ID}"
-        write_log "Unknown Stream:${STREAM_ID}"
+echo "Unknown Stream:${STREAM_NAME}"
+        write_log "Unknown Stream:${STREAM_NAME}"
         exit 98
 fi
 
 if [ "${LAST_ALARM}" != "" ]; then
+  LAST_ALARM=$(date -d"${LAST_ALARM}" +%s)
 	ALARM_AGE=$(expr $(date +%s) - ${LAST_ALARM})
-    ALARM_TIME=$(date --date="${ALARM_AGE} seconds ago" -u )
+  ALARM_TIME=$(date --date="${ALARM_AGE} seconds ago" -u )
 else
 	ALARM_AGE=""
 fi
@@ -168,13 +169,15 @@ else
 fi
 
 #Get performance data
-PERF_STREAM_URL="${SERVER_URL}/streams/${STREAM_ID}/messages.json?api_key=${API_KEY}"
-get_streams_json "${PERF_STREAM_URL}" "${GRAYLOG2_STREAMS_FILE}"
-PERF_VALUE=$(get_perf_count "${GRAYLOG2_STREAMS_FILE}")
+PERF_STREAM_URL="${SERVER_URL}/streams/${STREAM_ID}/alerts"
+get_streams_json "${PERF_STREAM_URL}" "${GRAYLOG2_STREAMS_FILE}" "-u ${API_KEY}:token"
+PERF_VALUE=$(get_perf_count ${GRAYLOG2_STREAMS_FILE})
 rm -rf ${GRAYLOG2_STREAMS_FILE}
 
-write_log "Alarm:${ALARM_STATE} - Stream:${STREAM_ID} Last-alarm:${ALARM_TIME} Invert:$INVERT | avg-msg-min=${PERF_VALUE}"
+STREAM_NAME=$(curl -k -s -u 11b9i6j0lteqpv30gr7l81pp51lt7d6l3ehd3jm81sahfi4be4d6:token https://an-log-v1-nd.angers.perf1.net/api/streams/${STREAM_ID}/ | jq .title )
+write_log "Alarm:${ALARM_STATE} - Stream:${STREAM_NAME} Last-alarm:${ALARM_TIME} Invert:$INVERT | avg-msg-min=${PERF_VALUE}"
 write_log "EXIT_CODE=${EXIT_CODE}"
 
-echo "Alarm:${ALARM_STATE} - Stream:${STREAM_ID} Last-alarm:${ALARM_TIME} | avg-msg-min=${PERF_VALUE};"
+
+echo "Alarm:${ALARM_STATE} - Stream:${STREAM_NAME} Last-alarm:${ALARM_TIME} | avg-msg-min=${PERF_VALUE};"
 exit ${EXIT_CODE}
